36bca9e886fa39ab418a0eccecaf638bb9937d10
JeanMaxSKrebs <jeanmaxskrebs@gmail.com>
2023-06-14 15:41:47 -0300

    fix: algumas alteracoes routes

--- src/App.js
+++ src/App.js
@@ -4,22 +4,64 @@ import FormSalao from './components/Saloes/FormSalao';
 import MostraSaloes from './components/Saloes/MostraSaloes';
 import TopNav from './components/Estrutura/TopNav';
 import SideNav from './components/Estrutura/SideNav';
+import { BrowserRouter as Router, Routes, Link, Route} from 'react-router-dom';
+
+  // Componentes das telas
+  export const Home = () => {
+    <>
+      <h1>Página inicial</h1>;
+        <div className="saloes-header">
+          <h1>Salões</h1>
+        </div>
+      <MostraSaloes /> 
+    </>
+  }
+  export const Sobre = () => {
+        // Resto do código do componente
+        <h1>Sobre</h1>;
+  };
+  export const Contato = () => <h1>Contato</h1>;
+  export const Cadastro = () => <h1>Cadastro</h1>;
+  export const Login = () => <h1>Login</h1>;
 
 
 function App() {
   return (
     <>
-      <TopNav />
-      <SideNav />
       <div className="App">
-        <div className="saloes-header">
-          <h1>Salões</h1>
-        </div>
+        <Router>
+                {/* <TopNav />
+        <SideNav /> */}
 
-        <FormSalao />
 
-        <MostraSaloes />
+          {/* <FormSalao />
 
+          <MostraSaloes /> */}
+
+        {/* </div> */}
+          <div>
+            {/* Conteúdo compartilhado em todas as rotas */}
+            <SideNav />
+            <header>
+              {/* Componentes do cabeçalho */}
+              <TopNav />
+            </header>
+
+            {/* Definição das rotas */}
+            <Routes>            
+              <Route path="/home" element={<Home />} />
+              <Route path="/sobre" element={<Sobre />} />
+              <Route path="/contato" element={<Contato />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+            </Routes>
+
+            {/* Conteúdo compartilhado em todas as rotas */}
+            <footer>
+              {/* Componentes do rodapé */}
+            </footer>
+          </div>
+        </Router>
       </div>
     </>
   );
